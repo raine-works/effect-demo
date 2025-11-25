f7adb964e811afd2e4a4e829c1c0475feee44ab0
raine-works <raine@raineworks.com>
2025-11-25 21:13:57 +0000

    added prisma 7

---
+++ packages/database/env.d.ts
@@ -0,0 +1,5 @@
+declare namespace Bun {
+	interface Env {
+		DATABASE_URL: string;
+	}
+}
--- packages/database/src/index.ts
+++ packages/database/src/index.ts
@@ -1,69 +1,7 @@
-/** biome-ignore-all lint/suspicious/noShadowRestrictedNames: use effect */
-import * as BunPgClient from '@effect-demo/sql-bun/client';
-import * as Config from 'effect/Config';
-import * as Duration from 'effect/Duration';
-import * as Effect from 'effect/Effect';
-import { identity } from 'effect/Function';
-import * as Layer from 'effect/Layer';
-import * as Schedule from 'effect/Schedule';
-import * as String from 'effect/String';
+import { PrismaPg } from '@prisma/adapter-pg';
+import { PrismaClient } from '@/database/generated/client';
 
-export const pgConfig = {
-	transformQueryNames: String.camelToSnake,
-	transformResultNames: String.snakeToCamel,
-	types: {
-		114: {
-			to: 25,
-			from: [114],
-			parse: identity,
-			serialize: identity
-		},
-		1082: {
-			to: 25,
-			from: [1082],
-			parse: identity,
-			serialize: identity
-		},
-		1114: {
-			to: 25,
-			from: [1114],
-			parse: identity,
-			serialize: identity
-		},
-		1184: {
-			to: 25,
-			from: [1184],
-			parse: identity,
-			serialize: identity
-		},
-		3802: {
-			to: 25,
-			from: [3802],
-			parse: identity,
-			serialize: identity
-		}
-	}
-} as const;
-
-export const PgLive = Layer.unwrapEffect(
-	Effect.gen(function* () {
-		return BunPgClient.layer({
-			url: yield* Config.redacted('DATABASE_URL'),
-			...pgConfig
-		});
-	})
-).pipe((self) =>
-	Layer.retry(
-		self,
-		Schedule.identity<Layer.Layer.Error<typeof self>>().pipe(
-			Schedule.check((input) => input._tag === 'SqlError'),
-			Schedule.intersect(Schedule.exponential('1 second')),
-			Schedule.intersect(Schedule.recurs(2)),
-			Schedule.onDecision(([[_error, duration], attempt], decision) =>
-				decision._tag === 'Continue'
-					? Effect.logInfo(`Retrying database connection in ${Duration.format(duration)} (attempt #${++attempt})`)
-					: Effect.void
-			)
-		)
-	)
-);
+export const database = (connectionString: string) => {
+	const adapter = new PrismaPg({ connectionString });
+	return new PrismaClient({ adapter });
+};
